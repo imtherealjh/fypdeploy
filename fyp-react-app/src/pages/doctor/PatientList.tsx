7d1b3666e651ce043bb7b6f0119229bb2e04bc56
zteezy19 <83036830+zteezy19@users.noreply.github.com>
2023-04-21 03:02:49 +0800

    patientlists & details added
    added code and files neccessary to display patients detailed data. used placeholder data for now

--- fyp-react-app/src/pages/doctor/PatientList.tsx
+++ fyp-react-app/src/pages/doctor/PatientList.tsx
@@ -1,3 +1,31 @@
-export default function PatientList() {
-  return <>PATIENTLIST</>;
+import React from "react";
+import PatientDetails from "../../components/PatientDetails";
+
+function PatientListPage() {
+  const patient = {
+    name: "John Doe",
+    contactNumber: "+1 (234) 567-8910",
+    sex: "Male",
+    age: 35,
+    dateOfBirth: "1988-01-01",
+    weight: 70,
+    height: 175,
+    hospitalizedBefore: true,
+    lastHospitalizedDate: "2021-05-01",
+    takingMedication: false,
+    foodAllergies: "Nuts",
+    drugAllergies: "Penicillin",
+    bloodType: "A+",
+    medicalConditions: "Diabetes",
+    emergencyContact: "Jane Doe",
+    emergencyContactNumber: "+1 (987) 654-3210",
+  };
+
+  return (
+    <div>
+      <PatientDetails patient={patient} />
+    </div>
+  );
 }
+
+export default PatientListPage;
