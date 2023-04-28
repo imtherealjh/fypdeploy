4a6ca4b7a6ce7ea24cd90c991c17f20cc733cac1
tacobell88 <ikfzr@hotmail.com>
2023-04-28 17:07:25 +0800

    pls work you stupid shit - med record and queue

--- fyp-react-app/src/pages/patient/MedicalRecords.tsx
+++ fyp-react-app/src/pages/patient/MedicalRecords.tsx
@@ -18,5 +18,89 @@ interface MedicalRecord {
 }
 
 export default function MedicalRecords() {
-  return <>MedicalRecords</>;
+  const axiosPrivate = useAxiosPrivate();
+  const [medicalRecord, setMedicalRecord] = useState<MedicalRecord | null>(
+    null
+  );
+
+  useEffect(() => {
+    fetchMedicalRecord();
+  }, []);
+
+  const fetchMedicalRecord = async () => {
+    try {
+      const userId = "some-user-id"; // have to replace this with the information used to identify the user
+      const response = await axiosPrivate.get(
+        `/doctor/getByMedicalRecordsId?medicalRecordsId=${userId}`
+      );
+      setMedicalRecord(response.data);
+    } catch (error) {
+      console.error("Error fetching medical record:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Medical Records</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {medicalRecord
+                  ? `${medicalRecord.username}, ${medicalRecord.age}`
+                  : "-"}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Date of Birth</th>
+              <th>Weight</th>
+              <th>Height</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.dateOfBirth || "-"}</td>
+              <td>{medicalRecord?.weight || "-"}</td>
+              <td>{medicalRecord?.height || "-"}</td>
+            </tr>
+            <tr>
+              <th>Hospitalized Before</th>
+              <th>Current Medication</th>
+              <th>Food Allergies</th>
+            </tr>
+            <tr>
+              <td>
+                {medicalRecord
+                  ? medicalRecord.hospitalizedBefore
+                    ? "Yes"
+                    : "No"
+                  : "-"}
+              </td>
+              <td>{medicalRecord?.currentMedication || "-"}</td>
+              <td>{medicalRecord?.foodAllergies || "-"}</td>
+            </tr>
+            <tr>
+              <th>Drug Allergies</th>
+              <th>Blood Type</th>
+              <th>Medical Conditions</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.drugAllergies || "-"}</td>
+              <td>{medicalRecord?.bloodType || "-"}</td>
+              <td>{medicalRecord?.medicalConditions || "-"}</td>
+            </tr>
+            <tr>
+              <th>Emergency Contact</th>
+              <th>Emergency Contact Number</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.emergencyContact || "-"}</td>
+              <td>{medicalRecord?.emergencyContactNumber || "-"}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
--- fyp-react-app/src/pages/patient/Queue.tsx
+++ fyp-react-app/src/pages/patient/Queue.tsx
@@ -1,20 +1,106 @@
 import React, { useEffect, useState } from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
-interface Appointment {
-  clinic: string;
-  doctor: string;
-  date: string;
-  time: string;
-  queueNumber: string | null;
+interface MedicalRecord {
+  username: string;
+  age: number;
+  dateOfBirth: string;
+  weight: number;
+  height: number;
+  hospitalizedBefore: boolean;
+  currentMedication: string;
+  foodAllergies: string;
+  drugAllergies: string;
+  bloodType: string;
+  medicalConditions: string;
+  emergencyContact: string;
+  emergencyContactNumber: string;
 }
 
-interface QueueStatus {
-  asAt: string;
-  currentQueueNumber: string;
-  userQueueNumber: string | null;
-}
+export default function MedicalRecords() {
+  const axiosPrivate = useAxiosPrivate();
+  const [medicalRecord, setMedicalRecord] = useState<MedicalRecord | null>(
+    null
+  );
 
-export default function Queue() {
-  return <>Queue</>;
+  useEffect(() => {
+    fetchMedicalRecord();
+  }, []);
+
+  const fetchMedicalRecord = async () => {
+    try {
+      const userId = "some-user-id"; // have to replace this with the information used to identify the user
+      const response = await axiosPrivate.get(
+        `/doctor/getByMedicalRecordsId?medicalRecordsId=${userId}`
+      );
+      setMedicalRecord(response.data);
+    } catch (error) {
+      console.error("Error fetching medical record:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Medical Records</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {medicalRecord
+                  ? `${medicalRecord.username}, ${medicalRecord.age}`
+                  : "-"}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Date of Birth</th>
+              <th>Weight</th>
+              <th>Height</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.dateOfBirth || "-"}</td>
+              <td>{medicalRecord?.weight || "-"}</td>
+              <td>{medicalRecord?.height || "-"}</td>
+            </tr>
+            <tr>
+              <th>Hospitalized Before</th>
+              <th>Current Medication</th>
+              <th>Food Allergies</th>
+            </tr>
+            <tr>
+              <td>
+                {medicalRecord
+                  ? medicalRecord.hospitalizedBefore
+                    ? "Yes"
+                    : "No"
+                  : "-"}
+              </td>
+              <td>{medicalRecord?.currentMedication || "-"}</td>
+              <td>{medicalRecord?.foodAllergies || "-"}</td>
+            </tr>
+            <tr>
+              <th>Drug Allergies</th>
+              <th>Blood Type</th>
+              <th>Medical Conditions</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.drugAllergies || "-"}</td>
+              <td>{medicalRecord?.bloodType || "-"}</td>
+              <td>{medicalRecord?.medicalConditions || "-"}</td>
+            </tr>
+            <tr>
+              <th>Emergency Contact</th>
+              <th>Emergency Contact Number</th>
+            </tr>
+            <tr>
+              <td>{medicalRecord?.emergencyContact || "-"}</td>
+              <td>{medicalRecord?.emergencyContactNumber || "-"}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
