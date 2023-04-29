e4b1eb26dda948c49a950b86b80ddf960b9a8ca6
zteezy19 <83036830+zteezy19@users.noreply.github.com>
2023-04-29 21:19:44 +0800

    SetUp NurseRouting& Dashboard

---
+++ fyp-react-app/src/pages/nurse/Home.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+
+import { Calendar, DateObject } from "react-multi-date-picker";
+
+import PatientList from "../../components/PatientList";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+function Dashboard() {
+  const axiosPrivate = useAxiosPrivate();
+  const [value, setValue] = useState<any>(new DateObject());
+  const [data, setData] = useState<any>({
+    patientList: [],
+    noOfPatients: 0,
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    let controller = new AbortController();
+
+    const fetchData = async () => {
+      let response = await axiosPrivate.get(
+        `/doctor/getPatientsByDate?apptDate=${value}`,
+        {
+          signal: controller.signal,
+        }
+      );
+
+      isMounted && setData(response.data);
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [value]);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <div className="dashboard-container">
+        <div className="d-flex flex-wrap gap-2">
+          <div
+            style={{
+              background: "#737373",
+              color: "white",
+              margin: "1rem",
+              borderRadius: "1rem",
+              flex: "1 1 25%",
+            }}
+            className="col p-3 d-flex flex-column justify-content-center"
+          >
+            <span
+              style={{
+                fontSize: "1.2rem",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Visits for Today:
+            </span>
+            <span style={{ fontSize: "1.1rem", textAlign: "center" }}>
+              {data.noOfPatients}
+            </span>
+          </div>
+          <div className="col d-flex justify-content-center">
+            <Calendar
+              format="DD/MM/YYYY"
+              value={value}
+              onChange={(date: DateObject) => setValue(date.format())}
+            />
+          </div>
+        </div>
+        <div className="mt-3">
+          <PatientList apptDetails={data.patientList} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
