1203bb498ba7d1642649fac824838b04dc54a635
zteezy19 <83036830+zteezy19@users.noreply.github.com>
2023-05-06 19:50:46 +0800

    fixed app.yaml being in incorrect directory

--- Backend/src/test/java/com/uow/FYP_23_S1_11/Fyp23S111ApplicationTests.java
+++ Backend/src/test/java/com/uow/FYP_23_S1_11/Fyp23S111ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.uow.FYP_23_S1_11;
+//package com.uow.FYP_23_S1_11;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Fyp23S111ApplicationTests {
+//@SpringBootTest
+//class Fyp23S111ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
