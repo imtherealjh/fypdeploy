6f00008c510f2a7172cb21f77bbcdb6e5629388e
Ng Jun Hua <jhng032@mymail.sim.edu.sg>
2023-05-13 10:10:02 +0800

    Changed to prod profile

--- fyp-react-app/src/api/axios.tsx
+++ fyp-react-app/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://backendtestdeploy-production.up.railway.app/api";
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://backendtestdeploy-production.up.railway.app/api";
+// const BASE_URL = "http://localhost:8080/api";
 
 export default axios.create({
   baseURL: BASE_URL,
