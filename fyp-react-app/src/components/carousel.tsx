74b02b2c5e6c509bb426e99be66b6a29ae5b07ef
Ng Jun Hua <jhng032@mymail.sim.edu.sg>
2023-05-14 10:28:48 +0800

    Fixed?

--- fyp-react-app/src/components/carousel.tsx
+++ fyp-react-app/src/components/carousel.tsx
@@ -7,10 +7,10 @@ import "../css/carousel.css";
 import { CarouselItems } from "../hooks/types";
 
 export type Props = {
-  items: Array<CarouselItems>;
+  images: Array<CarouselItems>;
 };
 
-export default function Carousel({ items }: Props) {
+export default function Carousel({ images }: Props) {
   const carouselRef = useRef<HTMLDivElement>(null);
   let scrollWidthRef = useRef(0),
     prevClientXRef = useRef(0),
@@ -67,38 +67,15 @@ export default function Carousel({ items }: Props) {
           ref={carouselRef}
           onTouchStart={onTouchStart}
           onMouseDown={onTouchStart}
-          className="postition-relative carousel"
+          className="d-flex carousel gap-2"
         >
-          <div style={{ fontSize: "1.1rem" }} className="d-block w-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <div className="d-flex justify-content-end">
-              <span>John - 5 stars</span>
-            </div>
-          </div>
-          <div style={{ fontSize: "1.1rem" }} className="d-block w-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <div className="d-flex justify-content-end">
-              <span>John - 5 stars</span>
-            </div>
-          </div>
-          {items.map((item, idx) => (
-            <img className="rounded-circle" key={"carousel" + idx} />
+          {images.map((img, idx) => (
+            <img
+              className="rounded-circle"
+              key={"carousel" + idx}
+              src={img.imageSrc}
+              alt={img.imageAlt}
+            />
           ))}
         </div>
       </div>
