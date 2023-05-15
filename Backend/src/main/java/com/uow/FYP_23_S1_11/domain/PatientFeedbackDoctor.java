3a8bc6cc29779a16b669f42dfcbae22138332543
Ng Jun Hua <jhng032@mymail.sim.edu.sg>
2023-05-15 08:37:07 +0800

    Fixed where average rating isn't showing the correct value

--- Backend/src/main/java/com/uow/FYP_23_S1_11/domain/PatientFeedbackDoctor.java
+++ Backend/src/main/java/com/uow/FYP_23_S1_11/domain/PatientFeedbackDoctor.java
@@ -6,8 +6,6 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
